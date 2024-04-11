d7b7596579d3f011be956fec759a88efa9194914
joeun-01 <jjy020121@gmail.com>
2024-04-11 16:32:23 +0900

    KAN-83 [FEAT]: PostLike 도메인 세팅

--- src/main/java/com/example/lucky7postservice/src/like/api/PostLikeController.java
+++ src/main/java/com/example/lucky7postservice/src/like/api/PostLikeController.java
@@ -1,7 +1,14 @@
 package com.example.lucky7postservice.src.like.api;
 
 import com.example.lucky7postservice.src.like.application.PostLikeService;
+import com.example.lucky7postservice.utils.config.BaseException;
+import com.example.lucky7postservice.utils.config.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostLikeController {
     private final PostLikeService postLikeService;
 
-
+    /* 글 공감하기 API */
+    @Operation(summary = "글 공감 API", description="글에 좋아요를 누릅니다")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "GLB-ERR-005", description = "요청이 성공적으로 처리되었습니다."),
+            @ApiResponse(responseCode = "GLB-ERR-001", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "GLB-ERR-005", description = "입력값이 잘못되었습니다."),
+            @ApiResponse(responseCode = "GLB-ERR-006", description = "존재하지 않는 유저입니다."),
+            @ApiResponse(responseCode = "GLB-ERR-007", description = "존재하지 않는 블로그입니다."),
+            @ApiResponse(responseCode = "GLB-ERR-008", description = "존재하지 않는 글입니다.")
+    })
+    @PostMapping("/{postId}/like")
+    public BaseResponse<String> modifyPost(@PathVariable Long postId) throws BaseException {
+        return new BaseResponse<>(postLikeService.like(postId));
+    }
 }
--- src/main/java/com/example/lucky7postservice/src/like/application/PostLikeService.java
+++ src/main/java/com/example/lucky7postservice/src/like/application/PostLikeService.java
@@ -1,5 +1,12 @@
 package com.example.lucky7postservice.src.like.application;
 
+import com.example.lucky7postservice.src.like.domain.PostLike;
+import com.example.lucky7postservice.src.like.domain.repository.PostLikeRepository;
+import com.example.lucky7postservice.src.post.domain.Post;
+import com.example.lucky7postservice.src.post.domain.PostState;
+import com.example.lucky7postservice.src.post.domain.repository.PostRepository;
+import com.example.lucky7postservice.utils.config.BaseException;
+import com.example.lucky7postservice.utils.config.BaseResponseStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PostLikeService {
+    private final PostRepository postRepository;
+    private final PostLikeRepository postLikeRepository;
+
+    @Transactional
+    public String like(Long postId) throws BaseException {
+        // TODO : 멤버 존재 여부 확인
+        // 글 존재 여부 확인
+        Post post = postRepository.findByIdAndPostState(postId, PostState.ACTIVE)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.INVALID_POST));
+
+        Long memberId = 1L;
+        postLikeRepository.save(PostLike.of(memberId, post));
+
+        return "좋아요를 눌렀습니다.";
+    }
 }

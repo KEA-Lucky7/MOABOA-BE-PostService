29911f2429ddb710f506cc36f74d53e9bacb3e62
joeun-01 <jjy020121@gmail.com>
2024-04-15 20:45:03 +0900

    KAN-93 [FEAT]: 댓글 등록 API

--- src/main/java/com/example/lucky7postservice/src/comment/api/CommentController.java
+++ src/main/java/com/example/lucky7postservice/src/comment/api/CommentController.java
@@ -1,6 +1,13 @@
 package com.example.lucky7postservice.src.comment.api;
 
+import com.example.lucky7postservice.src.comment.api.dto.PostCommentReq;
+import com.example.lucky7postservice.src.comment.api.dto.PostCommentRes;
 import com.example.lucky7postservice.src.comment.application.CommentService;
+import com.example.lucky7postservice.utils.config.BaseException;
+import com.example.lucky7postservice.utils.config.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,5 +17,18 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
     private final CommentService commentService;
 
-
+    /* 댓글 달기 API */
+    @Operation(summary = "댓글 등록 API", description="글에 댓글을 답니다")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "GLB-ERR-005", description = "요청이 성공적으로 처리되었습니다."),
+            @ApiResponse(responseCode = "GLB-ERR-001", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "GLB-ERR-005", description = "입력값이 잘못되었습니다."),
+            @ApiResponse(responseCode = "GLB-ERR-006", description = "존재하지 않는 유저입니다."),
+            @ApiResponse(responseCode = "GLB-ERR-007", description = "존재하지 않는 블로그입니다."),
+            @ApiResponse(responseCode = "GLB-ERR-008", description = "존재하지 않는 글입니다.")
+    })
+    @PostMapping("/{postId}/comment")
+    public BaseResponse<PostCommentRes> comment(@PathVariable Long postId, @RequestBody PostCommentReq postCommentReq) throws BaseException {
+        return new BaseResponse<>(commentService.comment(postId, postCommentReq));
+    }
 }
--- src/main/java/com/example/lucky7postservice/src/comment/application/CommentService.java
+++ src/main/java/com/example/lucky7postservice/src/comment/application/CommentService.java
@@ -1,6 +1,19 @@
 package com.example.lucky7postservice.src.comment.application;
 
+import com.example.lucky7postservice.src.comment.api.dto.PostCommentReq;
+import com.example.lucky7postservice.src.comment.api.dto.PostCommentRes;
+import com.example.lucky7postservice.src.comment.domain.Comment;
 import com.example.lucky7postservice.src.comment.domain.repository.CommentRepository;
+import com.example.lucky7postservice.src.post.api.dto.PostPostReq;
+import com.example.lucky7postservice.src.post.api.dto.PostPostRes;
+import com.example.lucky7postservice.src.post.api.dto.WalletReq;
+import com.example.lucky7postservice.src.post.domain.Hashtag;
+import com.example.lucky7postservice.src.post.domain.Post;
+import com.example.lucky7postservice.src.post.domain.PostState;
+import com.example.lucky7postservice.src.post.domain.Wallet;
+import com.example.lucky7postservice.src.post.domain.repository.PostRepository;
+import com.example.lucky7postservice.utils.config.BaseException;
+import com.example.lucky7postservice.utils.config.BaseResponseStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +22,18 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CommentService {
+    private final PostRepository postRepository;
     private final CommentRepository commentRepository;
 
+    @Transactional
+    public PostCommentRes comment(Long postId, PostCommentReq commentReq) throws BaseException {
+        // TODO : 멤버 존재 여부 확인
+        Post post = postRepository.findByIdAndPostState(postId, PostState.ACTIVE).orElseThrow(
+                () -> new BaseException(BaseResponseStatus.INVALID_POST));
+
+        String content = commentReq.getContent().trim();
+        Comment comment = commentRepository.save(Comment.of(1L, post, content));
+
+        return new PostCommentRes(comment.getId());
+    }
 }

5ebf143411d5e6612d567c0a9a9fbd850e19a690
joeun-01 <jjy020121@gmail.com>
2024-05-23 17:02:06 +0900

    KAN-70 [MODIFY]: 글 수정 API PostType 수정

--- src/main/java/com/example/lucky7postservice/src/command/post/api/dto/PostSavedPostReq.java
+++ src/main/java/com/example/lucky7postservice/src/command/post/api/dto/PostSavedPostReq.java
@@ -18,7 +18,7 @@ public class PostSavedPostReq {
     private String content;
     @NotBlank(message="자유글/가계부 여부를 입력해야 합니다.")
     @Pattern(regexp = "^(FREE|WALLET)$", message = "FREE 혹은 WALLET으로 입력해야 합니다")
-    @Schema(description = "자유글/가계부 여부", example = "free/wallet")
+    @Schema(description = "자유글/가계부 여부", example = "FREE/WALLET")
     private String postType;
     @Schema(description = "해시태그 리스트", example = "야구, 운동, 소비기록")
     private List<String> hashtagList;

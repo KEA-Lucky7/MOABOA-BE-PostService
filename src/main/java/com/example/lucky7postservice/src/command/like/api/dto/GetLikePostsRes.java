c7999d37b62dd92891df06df60de020c93b07d26
joeun-01 <jjy020121@gmail.com>
2024-05-24 09:34:33 +0900

    KAN-220 [MODIFY]: 좋아요한 글 목록 반환 API blogId 추가

--- src/main/java/com/example/lucky7postservice/src/command/like/api/dto/GetLikePostsRes.java
+++ src/main/java/com/example/lucky7postservice/src/command/like/api/dto/GetLikePostsRes.java
@@ -14,6 +14,8 @@ public class GetLikePostsRes {
     private String thumbnail;
     @Schema(description = "게시물 대표 해시태그", example = "야구")
     private String mainHashtag;
+    @Schema(description = "작성자 블로그 아이디", example = "1")
+    private Long blogId;
     @Schema(description = "작성자 아이디", example = "1")
     private Long memberId;
     @Schema(description = "작성자 닉네임", example = "조은")
@@ -27,12 +29,13 @@ public class GetLikePostsRes {
 
     @Builder
     public GetLikePostsRes(Long postId, String title, String thumbnail, String mainHashtag,
-                           Long memberId, String nickname,
+                           Long blogId, Long memberId, String nickname,
                            int commentCnt, int likeCnt, String createdAt) {
         this.postId = postId;
         this.title = title;
         this.thumbnail = thumbnail;
         this.mainHashtag = mainHashtag;
+        this.blogId = blogId;
         this.memberId = memberId;
         this.nickname = nickname;
         this.commentCnt = commentCnt;

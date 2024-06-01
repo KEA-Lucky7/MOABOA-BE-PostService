c4824016242dfe88829310493ec8d3f83358fa9a
joeun-01 <jjy020121@gmail.com>
2024-06-01 13:17:50 +0900

    KAN-220 [MODIFY]: 좋아요한 글 목록 조회 API 총 글 개수 추가

--- src/main/java/com/example/lucky7postservice/src/command/like/api/dto/GetLikePostsRes.java
+++ src/main/java/com/example/lucky7postservice/src/command/like/api/dto/GetLikePostsRes.java
@@ -1,28 +1,14 @@
 package com.example.lucky7postservice.src.command.like.api.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-public interface GetLikePostsRes {
-    @Schema(description = "게시물 아이디", example = "1")
-    Long getPostId();
-    @Schema(description = "게시물 제목", example = "제목입니다")
-    String getTitle();
-    @Schema(description = "게시물 대표 사진", example = "링크")
-    String getThumbnail();
-    @Schema(description = "게시물 대표 해시태그", example = "야구")
-    String getMainHashtag();
-    @Schema(description = "작성자 블로그 아이디", example = "1")
-    Long getBlogId();
-    @Schema(description = "작성자 아이디", example = "1")
-    Long getMemberId();
-    @Schema(description = "작성자 닉네임", example = "조은")
-    String getNickname();
-    @Schema(description = "댓글 개수", example = "3")
-    int getCommentCnt();
-    @Schema(description = "좋아요 개수", example = "70")
-    int getLikeCnt();
-    @Schema(description = "게시물 작성 날짜", example = "24.05.16")
-    String getCreatedAt();
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class GetLikePostsRes {
+    private Long memberId;
+    private int postCnt;
+    private List<LikePostsRes> postList;
 }

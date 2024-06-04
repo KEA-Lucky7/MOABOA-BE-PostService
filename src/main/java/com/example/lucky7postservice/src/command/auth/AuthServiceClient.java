dfbc284e64a88a8d3485f2915f8cac79f2a575ff
joeun-01 <jjy020121@gmail.com>
2024-06-04 22:12:59 +0900

    KAN-93 [FEAT]: 댓글 달기 API JWT 적용

---
+++ src/main/java/com/example/lucky7postservice/src/command/auth/AuthServiceClient.java
@@ -0,0 +1,12 @@
+package com.example.lucky7postservice.src.command.auth;
+
+import com.example.lucky7postservice.utils.openfeign.HeaderConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "auth-service", configuration = HeaderConfig.class)
+public interface AuthServiceClient {
+    @PostMapping("/auth/token/server-validation")
+    ResponseEntity<Long> validateToken();
+}
---
+++ src/main/java/com/example/lucky7postservice/utils/openfeign/HeaderConfig.java
@@ -0,0 +1,27 @@
+package com.example.lucky7postservice.utils.openfeign;
+
+import feign.RequestInterceptor;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+public class HeaderConfig {
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return template -> {
+            RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+
+            if (requestAttributes != null) {
+                HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+
+                String token = request.getHeader("Authorization");
+                if (token != null && !token.isEmpty()) {
+                    template.header(HttpHeaders.AUTHORIZATION, token);
+                }
+            }
+        };
+    }
+}

3f529bdf3dfeaee1264fc7b9c29fb66a184d09e1
joeun-01 <jjy020121@gmail.com>
2024-06-05 03:48:45 +0900

    KAN-270 [DOCS]: FeignClient 오류 수정

--- src/main/java/com/example/lucky7postservice/Lucky7PostServiceApplication.java
+++ src/main/java/com/example/lucky7postservice/Lucky7PostServiceApplication.java
@@ -1,6 +1,8 @@
 package com.example.lucky7postservice;
 
+import com.example.lucky7postservice.utils.openfeign.HeaderConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
+@ImportAutoConfiguration({HeaderConfig.class})
 public class Lucky7PostServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Lucky7PostServiceApplication.class, args);
